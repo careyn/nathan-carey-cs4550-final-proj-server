c5efdb8bf63459b3d80017409484e80ae9f1d0c1
careyn <carey.n@northeastern.edu>
2022-12-08 18:21:43 -0500

    comment deletion

--- comments/comments-controller.js
+++ comments/comments-controller.js
@@ -1,24 +1,31 @@
-import * as dao from "./comments-dao.js"
+import * as commentsDao from "./comments-dao.js"
 
 const CommentsController = (app) => {
     const createComment = async (req, res) => {
         const comment = req.body
         const currentUser = req.session['currentUser']
         comment.author = currentUser._id
-        const actualComment = await dao.createComment(comment)
+        const actualComment = await commentsDao.createComment(comment)
         res.json(actualComment)
+    }
+    const deleteComment = async (req, res) => {
+        const cid = req.params['cid']
+        const status = await commentsDao.deleteComment(cid)
+        res.send(status)
     }
     const findCommentsByPokemon = async (req, res) => {
         const pokemon_name = req.params.pokemon_name
-        const comments = await dao.findCommentsByPokemon(pokemon_name)
+        const comments = await commentsDao.findCommentsByPokemon(pokemon_name)
         res.json(comments)
     }
     const findCommentsByAuthor = async (req, res) => {
         const author = req.params.author
-        const comments = await dao.findCommentsByAuthor(author)
+        const comments = await commentsDao.findCommentsByAuthor(author)
         res.json(comments)
     }
     app.post('/api/comments', createComment)
+    app.delete('/comment/:cid', deleteComment)
+
     app.get('/api/pokemon/:pokemon_name/comments', findCommentsByPokemon)
     app.get('/api/users/:author/comments', findCommentsByAuthor)
 }
--- comments/comments-dao.js
+++ comments/comments-dao.js
@@ -3,6 +3,11 @@ import commentsModel from "./comments-model.js";
 export const createComment = (comment) =>
     commentsModel.create(comment)
 
+export const deleteComment = async (cid) => {
+    const status = await commentsModel.deleteOne({_id: cid})
+    return status
+}
+
 export const findCommentsByPokemon = (pokemon_name) =>
     commentsModel
         .find({pokemon_name})
